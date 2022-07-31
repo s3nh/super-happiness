b6e5a7e30648b77db4bbbaf6d943375e53726b64
s3nh <17111860+s3nh@users.noreply.github.com>
2022-07-31 14:35:07 +0200

    Update nvidia-cuda-installation.sh

--- nvidia-cuda-installation.sh
+++ nvidia-cuda-installation.sh
@@ -13,6 +13,9 @@ sudo apt-get install linux-headers-$(uname -r)
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
+#In calse of ' The public CUDA GPG key does not appear to be installed
+sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-46B62B5F-keyring.gpg /usr/share/keyrings/
+
 sudo dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get updatesudo apt-get -y install cuda
